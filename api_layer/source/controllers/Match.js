43dc7e0015027cf928f3625ba347a974bc21296a
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-09-23 23:53:10 +0200

    GLM-30 Add deletion to Match controller
    Added the deletion to the match controller.
    However, I am getting some sporadic failures in my test.
    Need to look into it more.

---
+++ api_layer/source/controllers/Match.js
@@ -0,0 +1,27 @@
+const MatchModel = require('../models/Match');
+const errors = require('../utility/error');
+
+
+module.exports = (api) => {
+    api.route('/Match/:id')
+       .get((req, res) => {
+            let id = req.params.id;
+            MatchModel.findById(id, (err, match) => {
+                    const errc = (match) ? 200 : 404;
+                    const val = (match) ? match : {};
+                    res.status(errc).json(match);
+            })
+            .catch(err => res.status(500).json(errors.ERROR_500));
+       });
+
+    api.route('/Match/:id')
+       .delete((req, res) => {
+            const id = req.params.id;
+            MatchModel.findByIdAndRemove(id, (err, match) => {
+                const errc = (match) ? 200 : 202;
+                const val = (match) ? match : {};
+                res.status(errc).json(val);
+            })
+            .catch(err => res.status(500).json(errors.ERROR_500));
+       });
+}
---
+++ api_layer/source/utility/error.js
@@ -0,0 +1,11 @@
+module.exports = {
+    ERROR_500: {
+        code: 1,
+        message: 'Error 500 internal server error',
+    },
+
+    INVALID_INPUT: {
+        code: 3,
+        message: 'Invalid user input',
+    }
+}
