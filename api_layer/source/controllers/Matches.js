18f2664df6311681346563da39da0657ab8a524c
Andreas Wang <andrwan@stud.ntnu.no>
2017-09-24 17:13:41 +0200

    GLM-16 Add GET request for gameToken Added the GET request that returns a list of matches with provided gameToken.

--- api_layer/source/controllers/Matches.js
+++ api_layer/source/controllers/Matches.js
@@ -7,4 +7,19 @@ module.exports = (api) => {
                 .then(list => res.json(list))
                 .catch(err => res.json(err));
         });
+
+    /**
+     * @api {get} /Match/:gameToken Request matches with given gameToken
+     * @apiName GetMatches
+     * @apiGroup Matches
+     *
+     * @apiParam {String} gameToken The unique game token of a game.
+     *
+     */
+    api.route('/Matches/:gameToken')
+         .get((req, res) => {
+            MatchModel.findByToken(req.params.gameToken)
+                .then(list=> res.json(list))
+                .catch(err => res.json(err));
+        });
 };
