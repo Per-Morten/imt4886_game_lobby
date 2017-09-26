d6e430c80f340a4e7df2a44ab50410105f3574a7
andrwan@stud.ntnu.no <andrwan@stud.ntnu.no>
2017-09-26 11:13:36 +0200

    GLM-25 Add example use of GET for match list. Added an example of how to do a GET request in the Unity example and display the acquired matches.
     #time 1h 5m

---
+++ application_examples/unity/lobby_demo/Assets/Scripts/KJAPPNetworkManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class KJAPPNetworkManager : NetworkManager
+{
+    [Header("KJAPP specific")]
+    public string gameToken;
+    public string apiUrl;
+
+    public void StartHosting()
+    {
+        StartCoroutine(UploadMatch());
+    }
+
+    public void StartClientConnection(string ip, int port)
+    {
+        networkAddress = ip;
+        networkPort = port;
+
+        StartClient();
+    }
+
+    public void RequestMatches()
+    {
+        StartCoroutine(FetchMatches());
+    }
+
+    private IEnumerator UploadMatch()
+    {
+        Match matchPOSTData = new Match(gameToken, 0, networkAddress, networkPort);
+
+        // https://forum.unity.com/threads/unitywebrequest-post-url-jsondata-sending-broken-json.414708/
+        UnityWebRequest webRequest = UnityWebRequest.Put(apiUrl + "/Match/" + gameToken, JsonUtility.ToJson(matchPOSTData) ?? "");
+        webRequest.SetRequestHeader("Content-Type", "application/json");
+        yield return webRequest.Send();
+
+        if (webRequest.isNetworkError)
+        {
+            Debug.Log(webRequest.error);
+        } 
+        else
+        {
+            StartHost();
+        }
+    }
+
+    private IEnumerator FetchMatches()
+    {
+        // https://docs.unity3d.com/Manual/UnityWebRequest-RetrievingTextBinaryData.html
+        UnityWebRequest webRequest = UnityWebRequest.Get(apiUrl + "/Matches/" + gameToken);
+
+        yield return webRequest.Send();
+
+        if (webRequest.isNetworkError)
+        {
+            Debug.Log(webRequest.error);
+        }
+        else
+        {
+            string jsonString = webRequest.downloadHandler.text;
+            Match[] matches = JsonHelper.getJsonArray<Match>(jsonString);
+            GameObject.FindGameObjectWithTag("UIHandler").GetComponent<UIHandler>().DisplayMatches(matches);
+        }
+    }
+}
---
+++ application_examples/unity/lobby_demo/Assets/Scripts/Match.cs
@@ -0,0 +1,17 @@
+﻿[System.Serializable]
+public class Match
+{
+    public string gameToken = null;
+    public int status = -1;
+    public string hostIP = null;
+    public int hostPort = -1;
+    public string id = null;
+
+    public Match(string token, int status, string IP, int port)
+    {
+        gameToken = token;
+        this.status = status;
+        hostIP = IP;
+        hostPort = port;
+    }
+}
