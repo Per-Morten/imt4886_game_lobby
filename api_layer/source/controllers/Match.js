2529fd665391996204d4eea9d349ed78a61e9b48
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-09-30 15:00:55 +0200

    GLM-30 Add documentation to getMatch

--- api_layer/source/controllers/Match.js
+++ api_layer/source/controllers/Match.js
@@ -3,7 +3,41 @@ const errors = require('../utility/error');
 
 
 module.exports = (api) => {
-    api.route('/Match/:id')
+    /**
+     * @api {get} /match/:id Get match with a specific id
+     * @apiName GetMatch
+     * @apiGroup Match
+     * @apiDescription
+     *  Gets the match with the specified id.
+     *  Returns 200 on success with the object,
+     *  404 if the match with requested id couldn't be found.
+     *
+     * @apiParam {Id} id The unique id of the match.
+     * @apiSuccess (200) _id The unique identifier of the match.
+     * @apiSuccess (200) __v The version of the match in the database.
+     * @apiSuccess (200) gameToken The identifier of the game the match belongs to.
+     * @apiSuccess (200) status The current status of the game. 0 for waiting, 1 for in session.
+     * @apiSuccess (200) hostIP The ip address of the host of the match.
+     * @apiSuccess (200) hostPort The port that the match is run on.
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "_id": "59c7f0c9b0a0932165c058b6"
+     *       "__v": 0
+     *       "gameToken": "Game 1"
+     *       "status": 1
+     *       "hostIP": "127.0.0.0",
+     *       "hostPort": 3000
+     *     }
+     *
+     * @apiError (404) MatchNotFound The supplied ID was not found.
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {}
+     */
+    api.route('/match/:id')
        .get((req, res) => {
             let id = req.params.id;
             MatchModel.findById(id, (err, match) => {
