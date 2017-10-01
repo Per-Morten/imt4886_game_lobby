93034ccdc3c052801fd834cfa485f05fa5e52647
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-10-01 16:15:41 +0200

    GLM-17 minor fixup in documentation

--- api_layer/source/controllers/Match.js
+++ api_layer/source/controllers/Match.js
@@ -18,48 +18,35 @@ module.exports = (api) => {
 
 
   /**
-    * @api {put} /match/:id/:status update status of specified match
-    * @apiName UpdateMatchStatus
-    * @apiGroup Match
-    * @apiDescription
-    *   The status of the match with the specified ID is updated
-    *   Returns 204 on success,
-    *   404 if the match with requested ID couldn't be found ,
-    *   200 if the response says the status was updated
-    *
-    * @apiParam {Id} id the unique ID of the match.
-    * @apiParam {status} status the current status of the match. true if started. false if not.
-    *
-    * @apiSuccess (200) _id The unique identifier of the match.
-    * @apiSuccess (200) status The current status of the game. 0 for waiting, 1 for in session.
-    * @apiSuccess (204) Success
-
-     *
-     * @apiSuccessExample Success-Response:
-     *     HTTP/1.1 204 OK
-     *     {}
-     *
-     * @apiError (404) MatchNotFound The supplied ID was not found.
-     *
-     * @apiErrorExample Error-Response:
-     *     HTTP/1.1 404 Not Found
-     *     {}
-     *
-     *  @apiSuccess (200) MatchUpdated: Success-Response
-     *
-     * @apiSuccessExample Success-Response:
-     *     HTTP/1.1 200 OK
-     *     {}
-  */
-  api.route('/match/:id/:status')
-
+   * @api {put} /match/:id/:status update status of specified match
+   * @apiName UpdateMatchStatus
+   * @apiGroup Match
+   * @apiDescription
+   *   The status of the match with the specified ID is updated
+   *   Returns 204 on success,
+   *   404 if the match with requested ID couldn't be found.
+   *
+   * @apiParam {Id} id the unique ID of the match.
+   * @apiParam {Number={0..1}} status the current status of the match. true if started. false if not.
+   *
+   * @apiSuccess (204) Success
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 204 OK
+   *     {}
+   *
+   * @apiError (404) MatchNotFound The supplied ID was not found.
+   *
+   * @apiErrorExample Error-Response:
+   *     HTTP/1.1 404 Not Found
+   *     {}
+   */
+    api.route('/match/:id/:status')
        .put((req, res) => {
             const id = req.params.id;
             const status = req.params.status;
             MatchModel.updateStatus(id, status)
             .then(out => res.status(out.code).send())
             .catch(err => res.status(err.code).json(err));
-           });
+        });
 }
-
-// a new change
