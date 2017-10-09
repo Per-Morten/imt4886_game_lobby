0628b7225248433f5e302b80958cd7e5441e5c82
Andreas Wang <andrwan@stud.ntnu.no>
2017-10-09 10:11:02 +0200

    GLM-58 Add support for GET with params

--- api_layer/source/controllers/Matches.js
+++ api_layer/source/controllers/Matches.js
@@ -10,7 +10,7 @@ module.exports = (api) => {
 
     /**
      * @api {get} /matches/ Request matches with given gameToken
-     * @apiName GetMatches
+     * @apiName GetMatchesJSONBody
      * @apiGroup Matches
      * @apiDescription
      * Returns a JSON array of all matches that contain the provided gameToken.
@@ -27,9 +27,29 @@ module.exports = (api) => {
                 .catch(err => res.json(err));
         });
 
+
+    /**
+     * @api {get} /matches/:gameToken Request matches with given gameToken
+     * @apiName GetMatchesNoBody
+     * @apiGroup Matches
+     * @apiDescription
+     * Returns a JSON array of all matches that contain the provided gameToken.
+     * This array is empty if no matches were found.
+     *
+     * @apiParam {String} gameToken The unique game token of a game.
+     * @apiSuccess (200) Success
+     *
+     */
+    api.route('/matches/:gameToken')
+         .get((req, res) => {
+            MatchModel.findByToken(req.params.gameToken)
+                .then(list => res.json(list))
+                .catch(err => res.json(err));
+        });
+
     /**
      * @api {get} /matches/in_session/ Request matches that are in session with given gameToken
-     * @apiName GetMatchesInSession
+     * @apiName GetMatchesInSessionJSONBody
      * @apiGroup Matches
      * @apiDescription
      * Returns a list of all matches that are in session and contain the provided gameToken.
@@ -47,8 +67,27 @@ module.exports = (api) => {
         });
 
     /**
+     * @api {get} /matches/in_session/:gameToken Request matches that are in session with given gameToken
+     * @apiName GetMatchesInSessionNoBody
+     * @apiGroup Matches
+     * @apiDescription
+     * Returns a list of all matches that are in session and contain the provided gameToken.
+     * This list is empty if no matches were found.
+     *
+     * @apiParam {String} gameToken The unique game token of a game.
+     * @apiSuccess (200) Success
+     *
+     */
+    api.route('/matches/in_session/:gameToken')
+        .get((req, res) => {
+            MatchModel.findByTokenInSession(req.params.gameToken)
+                .then(list => res.json(list))
+                .catch(err => res.json(err));
+        });
+
+    /**
      * @api {get} /matches/not_full/ Request all the matches that are not full with a given gameToken
-     * @apiName GetMatchesNotFull
+     * @apiName GetMatchesNotFullJSONBody
      * @apiGroup Matches
      * @apiDescription
      * Returns a JSON array of all matches where playerCount < maxPlayerCount.
@@ -98,6 +137,62 @@ module.exports = (api) => {
     api.route('/matches/not_full')
         .get((req, res) => {
             MatchModel.findByTokenNotFull(req.body.gameToken)
+                .then(out => res.status(out.code).json(out.matches))
+                .catch(err => res.status(err.code).json(err));
+        });
+
+    /**
+     * @api {get} /matches/not_full/:gameToken Request all the matches that are not full with a given gameToken
+     * @apiName GetMatchesNotFullNoBody
+     * @apiGroup Matches
+     * @apiDescription
+     * Returns a JSON array of all matches where playerCount < maxPlayerCount.
+     * The array does not include matches that have not specified maxPlayerCount.
+     *
+     * @apiParam {String} gameToken The unique game token of a game.
+     * @apiSuccess (200) Success
+     *
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     [
+     *       { _id: '59da7d0e704a440b4fc6d840',
+     *         name: 'Test Match 2',
+     *         gameToken: '59da7d0e704a440b4fc6d83d',
+     *         status: 0,
+     *         hostIP: '127.0.0.1',
+     *         hostPort: 3000,
+     *         __v: 0,
+     *         maxPlayerCount: 9007199254740991,
+     *         playerCount: 1 },
+     *       { _id: '59da7d0e704a440b4fc6d842',
+     *         name: 'Test Match 4',
+     *         gameToken: '59da7d0e704a440b4fc6d83d',
+     *         status: 0,
+     *         hostIP: '127.0.0.1',
+     *         hostPort: 3000,
+     *         __v: 0,
+     *         maxPlayerCount: 36,
+     *         playerCount: 35 },
+     *       { _id: '59da7d0e704a440b4fc6d843',
+     *         name: 'Test Match 5',
+     *         gameToken: '59da7d0e704a440b4fc6d83d',
+     *         status: 0,
+     *         hostIP: '127.0.0.1',
+     *         hostPort: 3000,
+     *         __v: 0,
+     *         maxPlayerCount: 36,
+     *         playerCount: 22 }
+     *      ]
+     *
+     * @apiError (404) TokenNotFound The supplied GameToken was not found.
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {}
+     */
+    api.route('/matches/not_full/:gameToken')
+        .get((req, res) => {
+            MatchModel.findByTokenNotFull(req.params.gameToken)
                 .then(out => res.status(out.code).json(out.matches))
                 .catch(err => res.status(err.code).json(err));
         });
