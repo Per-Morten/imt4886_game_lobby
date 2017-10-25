4e8a16924faff3248de2443c219db2fd15c3c63f
Per-Morten Straume <Gamma_ray_per@hotmail.com>
2017-10-25 12:32:27 +0200

    GLM-63 Add hostMatch functionality
    Add hostMatch functionality to kjapp api.

--- application_examples/cpp/chat_demo/chat_demo/kjapp.cpp
+++ application_examples/cpp/chat_demo/chat_demo/kjapp.cpp
@@ -1,119 +1,116 @@
 #include "kjapp.h"
-#include <cstdio>
-#include <SDL_net.h>
-#include <cstring>
+
 #include <cinttypes>
+#include <cstdio>
+#include <cstring>
+#include <memory>
 
-void kjapp::hostMatch(const char* gameToken, 
-                      const char* name, 
-                      const char* hostIp, 
-                      const std::uint16_t hostPort, 
-                      const std::size_t maxPlayerCount)
+#include <curl/curl.h>
+#include "json.hpp"
+
+struct CurlDeleter
 {
-    // For format, look at:
-    // https://www.ibm.com/support/knowledgecenter/en/SSSHTQ_8.1.0/com.ibm.netcool_OMNIbus.doc_8.1.0/omnibus/wip/api/reference/omn_api_http_examplehttp.html
-    IPaddress kjappIp;
-    if (SDLNet_ResolveHost(&kjappIp, "up.imt.hig.no", 8333) == -1)
+    void operator()(CURL* curl)
     {
-        std::printf("Could not resolve\n");
+        if (curl)
+        {
+            curl_easy_cleanup(curl);
+        }
+    }
+};
+
+using CurlHandle = std::unique_ptr<CURL, CurlDeleter>;
+
+CurlHandle
+createCurlHandle()
+{
+    CURL* curl = curl_easy_init();
+    if (!curl)
+    {
+        throw std::runtime_error("Could not init curl");
     }
 
-//    const auto request = "POST /match/ HTTP/1.1\r\n"
-//                         //"Host: up.imt.hig.no\r\n"
-//                         //"Content-Type: application/json\r\n"
-//                         "\r\n";
-//                         "{\r\n"
-//                         "  gameToken: \"59ec8be7890cd692461bb7d4\",\r\n"
-//                         "  name: \"TestMatch\",\r\n"
-//                         "  hostIP: \"localhost\",\r\n"
-//        "   hostPort: 8800,\r\n"
-//        "   maxPlayerCount: 0\r\n"
-//        "}\r\n";
+    return CurlHandle(curl);
+}
 
-    const auto request = "GET / HTTP/1.1\r\n"
-                         "Host: up.imt.hig.no\r\n"
-        //"Accept: application/json\r\n"
-        //"Connection: keep-alive\r\n"
-        //"Content-Type: application/json\r\n"
-        "\r\n";
-    
-    auto socket = SDLNet_TCP_Open(&kjappIp);
-    auto address = SDLNet_TCP_GetPeerAddress(socket);
-    printf("%" PRIu8 ".%" PRIu8 ".%" PRIu8 ".%" PRIu8 ":%" PRIu16 "\n",
-           reinterpret_cast<std::uint8_t*>(&address->host)[0],
-           reinterpret_cast<std::uint8_t*>(&address->host)[1],
-           reinterpret_cast<std::uint8_t*>(&address->host)[2],
-           reinterpret_cast<std::uint8_t*>(&address->host)[3],
-           address->port);
+using json = nlohmann::json;
 
-    if (!socket)
+namespace
+{
+    namespace local
     {
-        std::printf("Could not connect\n");
-    }
-    std::printf("Connected\n");
+        std::size_t
+        hostMatchCallback(void* contents, std::size_t size, std::size_t nmemb, void* userData)
+        {
+            std::size_t realSize = size * nmemb;
 
-    SDLNet_TCP_Send(socket, request, std::strlen(request) + 1);
-    
-    while (true)
+            // Contents isn't null terminated, and I don't want to rely on Json library there,
+            // so just ensuring that the std::string never reads more than realSize number of characters.
+            std::string jsonString(static_cast<char*>(contents), realSize);
+            nlohmann::json* output = static_cast<nlohmann::json*>(userData);
+            *output = json(jsonString);
+            return size * nmemb;
+        };
+    }
+}
+
+nlohmann::json
+kjapp::hostMatch(const std::string& gameToken,
+                 const std::string& name,
+                 const std::string& hostIP,
+                 const std::uint16_t hostPort,
+                 const std::size_t maxPlayerCount)
+{
+    return hostMatch(gameToken, name, hostIp, hostPort, maxPlayerCount, " ");
+}
+
+nlohnmann::json
+kjapp::hostMatch(const std::string& gameToken,
+                 const std::string& name,
+                 const std::string& hostIp,
+                 const std::uint16_t hostPort,
+                 const std::size_t maxPlayerCount,
+                 const nlohmann::json& miscInfo)
+{
+    auto handle = createCurlHandle();
+
+    // Setup callback
+    curl_easy_setopt(handle.get(), CURLOPT_WRITEFUNCTION, local::hostMatchCallback);
+
+    nlohmann::json output;
+    curl_easy_setopt(handle.get(), CURLOPT_WRITEDATA, &output);
+
+    // Prepare for post call
+    nlohmann::json package =
     {
-        char buffer[2048];
-        int received = SDLNet_TCP_Recv(socket, buffer, 2048);
-        printf("Received: %d bytes, %s\n", received, buffer);
-        SDL_Delay(1000);
+        {"gameToken", gameToken},
+        {"name", name},
+        {"hostIP", hostIp},
+        {"hostPort", hostPort},
+        {"maxPlayerCount", maxPlayerCount},
+        {"miscInfo", miscInfo},
+    };
+
+    auto jsonString = package.dump();
+    curl_easy_setopt(handle.get(), CURLOPT_POSTFIELDS, jsonString.data());
+    curl_easy_setopt(handle.get(), CURLOPT_POSTFIELDSIZE, jsonString.size());
+
+    // Set content type
+    curl_slist* headers = nullptr;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    curl_easy_setopt(handle.get(), CURLOPT_HTTPHEADER, headers);
+
+    // Set address
+    curl_easy_setopt(handle.get(), CURLOPT_URL, "up.imt.hig.no:8333/match/");
+
+    // Perform the request
+    const auto res =  curl_easy_perform(handle.get());
+    if (res != CURLE_OK)
+    {
+        curl_slist_free_all(headers);
+        throw std::runtime_error(curl_easy_strerror(res));
     }
 
-    SDLNet_TCP_Close(socket);
-
-    //constexpr int channel = 0;
-    //auto socket = SDLNet_UDP_Open(8880);
-    //if (!socket)
-    //{
-    //    printf("Couldnt create socket\n");
-    //    return;
-    //}
-    //auto result = SDLNet_UDP_Bind(socket, channel, &kjappIp);
-    //if (result == -1)
-    //{
-    //    printf("Could not bind socket\n");
-    //    SDLNet_UDP_Close(socket);
-    //    return;
-    //}
-
-    //#ifdef _MSC_VER
-    //#pragma warning(push)
-    //#pragma warning(disable : 4996)
-    //#endif
-    //int length = strlen(request) + 1;
-    //auto packet = SDLNet_AllocPacket(length);
-    //strcpy((char*)packet->data, request);
-    //packet->len = length;
-    //#ifdef _MSC_VER
-    //#pragma warning(pop)
-    //#endif
-
-    //auto res = SDLNet_UDP_Send(socket, channel, packet);
-    //printf("%d\n", res);
-    ////if (res == 0)
-    ////{
-    ////    printf("UDP error\n");
-    ////}
-
-    //auto recvPacket = SDLNet_AllocPacket(2048);
-    //int received = 0;
-    //while (received = SDLNet_UDP_Recv(socket, recvPacket) == 0)
-    //{
-    //    //printf("Waiting\n");
-    //}
-   
-    //if (received == -1)
-    //    printf("Error\n");
-    //else
-    //    printf("Received something: %s\n", recvPacket->data);
-
-    //SDLNet_FreePacket(packet);
-    //SDLNet_FreePacket(recvPacket);
-
-    //
-    //SDLNet_UDP_Unbind(socket, channel);
-    //SDLNet_UDP_Close(socket);
+    curl_slist_free_all(headers);
+    return output;
 }
--- application_examples/cpp/chat_demo/chat_demo/kjapp.h
+++ application_examples/cpp/chat_demo/chat_demo/kjapp.h
@@ -1,14 +1,24 @@
 #pragma once
 #include <cstddef>
 #include <cstdint>
+#include <string>
+#include <curl/curl.h>
+#include "json.hpp"
 
 namespace kjapp
 {
-    void
-    hostMatch(const char* gameToken,
-              const char* name,
-              const char* hostIp,
+    nlohmann::json
+    hostMatch(const std::string& gameToken,
+              const std::string& name,
+              const std::string& hostIP,
               const std::uint16_t hostPort,
               const std::size_t maxPlayerCount);
 
+    nlohmann::json
+    hostMatch(const std::string& gameToken,
+              const std::string& name,
+              const std::string& hostIP,
+              const std::uint16_t hostPort,
+              const std::size_t maxPlayerCount,
+              const nlohmann::json& miscInfo);
 }
